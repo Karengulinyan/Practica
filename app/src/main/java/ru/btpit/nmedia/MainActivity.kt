9d6bf32d88cf4d33adc7394bfa024d8cc4e5ef07
Danilkornienko <90alegator7@gmail.com>
2024-02-28 15:42:59 +0300

    six

--- app/src/main/java/ru/btpit/nmedia/MainActivity.kt
+++ app/src/main/java/ru/btpit/nmedia/MainActivity.kt
@@ -5,84 +5,90 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import ru.btpit.nmedia.databinding.ActivityMainBinding
+import ru.btpit.nmedia.databinding.CardPostBinding
 import ru.btpit.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
-
-
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val viewModel: PostViewModel by viewModels()
-        viewModel.data.observe(this) {post: Post ->
-            with(binding) {
-                textView1.text = post.author
-                textView2.text = post.published
-                textView3.text = post.content
-                imageView10.setImageResource(
-                    if (post.likedByMe) R.drawable.heart_svgrepo_com else R.drawable.hearth_svgrepo_com
-                )
-                textView9.text = post.likes.toString()
+        viewModel.data.observe(this) { posts ->
+            binding.container.removeAllViews()
+            posts.map { post ->
+                CardPostBinding.inflate(layoutInflater, binding.container, true).apply {
+                    textView1.text = post.author
+                    textView2.text = post.published
+                    textView3.text = post.content
+                    imageView10.setImageResource(
+                        if (post.likedByMe) R.drawable.heart_svgrepo_com else R.drawable.hearth_svgrepo_com
+                    )
+                    textView9.text = post.likes.toString()
 
-                if (post.likes >= 1000)
-                {
-                    textView9.text = "${post.likes / 1000}.${post.likes % 1000 / 100}К"
-                } else {
-                    textView9.text
-                }
-                if (post.likes >= 10000)
-                {
-                    textView9.text  ="${post.likes / 1000}.${post.likes % 1000 / 100}К"
-                } else{
-                    textView9.text
-                }
-                if (post.likes >= 1000000)
-                {
-                    textView9.text  = "${post.likes / 1000000}.${post.likes % 1000 / 100}М"
-                } else{
-                    textView9.text
-                }
 
-                binding.imageView10.setOnClickListener {
-                    viewModel.like()
-                }
+                    if (post.likes >= 1000)
+                    {
+                        textView9.text = "${post.likes / 1000}.${post.likes % 1000 / 100}К"
+                    } else {
+                        textView9.text
+                    }
+                    if (post.likes >= 10000)
+                    {
+                        textView9.text  ="${post.likes / 1000}.${post.likes % 1000 / 100}К"
+                    } else{
+                        textView9.text
+                    }
+                    if (post.likes >= 1000000)
+                    {
+                        textView9.text  = "${post.likes / 1000000}.${post.likes % 1000 / 100}М"
+                    } else{
+                        textView9.text
+                    }
+                    imageView10.setOnClickListener {
+                        viewModel.likeById(post.id)
+                    }
 
-                textView10?.text = post.share.toString()
-                if (post.share >= 1000)
-                {
-                    textView10.text = "${post.share / 1000}.${post.share % 1000 / 100}К"
-                } else {
-                    textView10.text
-                }
-                if (post.share >= 10000)
-                {
-                    textView10.text = "${post.share / 1000}.${post.share % 1000 / 100}К"
-                } else{
-                    textView10.text
-                }
-                if (post.share >= 1000000)
-                {
-                    textView10.text = "${post.share / 1000000}.${post.share % 1000 / 100}М"
-                } else{
-                    textView10.text
-                }
+                    textView10?.text = post.share.toString()
 
-                binding.imageView11.setOnClickListener {
-                    viewModel.share()
-                }
+
+                    if (post.share >= 1000)
+                    {
+                        textView10.text = "${post.share / 1000}.${post.share % 1000 / 100}К"
+                    } else {
+                        textView10.text
+                    }
+                    if (post.share >= 10000)
+                    {
+                        textView10.text = "${post.share / 1000}.${post.share % 1000 / 100}К"
+                    } else{
+                        textView10.text
+                    }
+                    if (post.share >= 1000000)
+                    {
+                        textView10.text = "${post.share / 1000000}.${post.share % 1000 / 100}М"
+                    } else{
+                        textView10.text
+                    }
+                    imageView11.setOnClickListener {
+                        viewModel.shareById(post.id)
+                    }
+
+                }.root
             }
-        }
 
-}}
+}}}
+
+
 data class Post(
     val id: Long,
     val author: String,
     val content: String,
     val published: String,
-    var likes: Int = 9999,
-    var share: Int = 9995,
+    var likes: Int = 99999,
+    var share: Int = 995,
     var likedByMe: Boolean = false,
     var shareByMe: Boolean = false
 )
--- app/src/main/java/ru/btpit/nmedia/Repository/PostRepositoryInMemoryImpl.kt
+++ app/src/main/java/ru/btpit/nmedia/Repository/PostRepositoryInMemoryImpl.kt
@@ -2,31 +2,58 @@ package ru.btpit.nmedia.Repository
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import ru.btpit.nmedia.MainActivity
 import ru.btpit.nmedia.Post
 
-class PostRepositoryInMemoryImpl: PostRepository {
-    private var post = Post(
-        id = 1,
-        author = "NIKE",
-        content = "Найди свой стиль танца на регулярных классах в Nike Box MSK. От хип-хопа до вога и от дэнсхолла до хауса – можно пробовать новые направления хоть каждый день. У нас есть занятия не только для взрослых, но и для самых юных танцоров. Все классы бесплатные, а уровень подготовки не имеет значения.",
-        published = "21 мая в 18:36",
-        likedByMe = false,
-        shareByMe = false
-    )
-    private val data = MutableLiveData(post)
 
-    override fun get(): LiveData<Post> = data
-    override fun like() {
-        post = post.copy(likedByMe = !post.likedByMe)
-        if (post.likedByMe) post.likes++ else post.likes--
-        data.value = post
+class PostRepositoryInMemoryImpl: PostRepository {
+    private var posts = listOf(
+        Post(
+            id = 2,
+            author = "NIKE",
+            content = "Найди свой стиль танца на регулярных классах в Nike Box MSK. От хип-хопа до вога и от дэнсхолла до хауса – можно пробовать новые направления хоть каждый день. У нас есть занятия не только для взрослых, но и для самых юных танцоров. Все классы бесплатные, а уровень подготовки не имеет значения.",
+            published = "5 мая в 18:36",
+            likedByMe = false,
+            shareByMe = false
+        ),
+       Post(
+            id = 1,
+            author = "NIKE",
+            content = "Танец – это язык твоего тела. И теперь ты сможешь овладеть им в совершенстве в Nike Box MSK. Здесь ты найдешь площадку для своих самых смелых танцевальных высказываний.",
+            published = "21 мая в 18:36",
+            likedByMe = false,
+            shareByMe = false
+        ),
+        )
+    private val data = MutableLiveData(posts)
+
+    override fun getAll(): LiveData<List<Post>> = data
+    override fun likeById(id : Long)
+    {
+         posts = posts.map{
+             if (it.id == id) {
+                 val likedByMe = !it.likedByMe
+                 val newLikesCount = if (likedByMe) it.likes + 1 else it.likes - 1
+                 it.copy(likedByMe = likedByMe, likes = newLikesCount)
+             } else {
+             it
+             }
+         }
+         data.value = posts
+
     }
 
-    override fun share()
+    override fun shareById(id: Long)
     {
-        post = post.copy(shareByMe = !post.shareByMe)
-        if (post.shareByMe) post.share++
-        data.value = post
+
+        posts = posts.map{
+            if (it.id == id) {
+                val shareByMe = !it.shareByMe
+                val newshareCount = it.share + 1
+                it.copy(shareByMe = shareByMe, share = newshareCount)
+            } else {
+                it
+            }
+        }
+        data.value = posts
     }
 }
