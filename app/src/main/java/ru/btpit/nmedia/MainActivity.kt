154396369607f3a75c51bbc163c43b3f7b82a4cb
Danilkornienko <90alegator7@gmail.com>
2024-03-05 10:10:47 +0300

    eight

--- app/src/main/java/ru/btpit/nmedia/MainActivity.kt
+++ app/src/main/java/ru/btpit/nmedia/MainActivity.kt
@@ -72,66 +72,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
-           //binding.container.removeAllViews()
-            /*posts.map { post ->
-                CardPostBinding.inflate(layoutInflater, binding.container, true).apply {
-                    textView1.text = post.author
-                    textView2.text = post.published
-                    textView3.text = post.content
-                    imageView10.setImageResource(
-                        if (post.likedByMe) R.drawable.heart_svgrepo_com else R.drawable.hearth_svgrepo_com
-                    )
-                    textView9.text = post.likes.toString()
-
-
-                    if (post.likes >= 1000)
-                    {
-                        textView9.text = "${post.likes / 1000}.${post.likes % 1000 / 100}К"
-                    } else {
-                        textView9.text
-                    }
-                    if (post.likes >= 10000)
-                    {
-                        textView9.text  ="${post.likes / 1000}.${post.likes % 1000 / 100}К"
-                    } else{
-                        textView9.text
-                    }
-                    if (post.likes >= 1000000)
-                    {
-                        textView9.text  = "${post.likes / 1000000}.${post.likes % 1000 / 100}М"
-                    } else{
-                        textView9.text
-                    }
-                    imageView10.setOnClickListener {
-                        viewModel.likeById(post.id)
-                    }
-
-                    textView10?.text = post.share.toString()
-
-
-                    if (post.share >= 1000)
-                    {
-                        textView10.text = "${post.share / 1000}.${post.share % 1000 / 100}К"
-                    } else {
-                        textView10.text
-                    }
-                    if (post.share >= 10000)
-                    {
-                        textView10.text = "${post.share / 1000}.${post.share % 1000 / 100}К"
-                    } else{
-                        textView10.text
-                    }
-                    if (post.share >= 1000000)
-                    {
-                        textView10.text = "${post.share / 1000000}.${post.share % 1000 / 100}М"
-                    } else{
-                        textView10.text
-                    }
-                    imageView11.setOnClickListener {
-                        viewModel.shareById(post.id)
-                    }
-                }.root
-            }*/
 
 data class Post(
     val id: Long,
